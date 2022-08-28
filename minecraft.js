9a1169ebc933215b772dad21d5b69c23a516418c
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-08-28 15:15:15 +0800

    Add files via upload

--- minecraft.js
+++ minecraft.js
@@ -25,7 +25,17 @@ const fs = require("fs");
 // 我自己 >> 清除前 764 KB | 清除後 241 KB
 if (option.player.DataClear) {
 	const Player = [];
+	const Ban = [];
 	const white = JSON.parse(fs.readFileSync(`${PATH}/whitelist.json`).toString());
+	const ban = JSON.parse(fs.readFileSync(`${PATH}/banned-players.json`).toString());
+	for (let index = 0; index < ban.length; index++) {
+		Ban.push(ban[index].uuid);
+		Ban.push(ban[index].name);
+	}
+	for (let index = 0; index < white.length; index++)
+		if (Ban.includes(white[index].uuid) || Ban.includes(white[index].name))
+			white.splice(0, 1);
+	fs.writeFileSync(`${PATH}/whitelist.json`, JSON.stringify(white));
 	for (let index = 0; index < white.length; index++)
 		Player.push(white[index].uuid);
 	const ops = JSON.parse(fs.readFileSync(`${PATH}/ops.json`).toString());
@@ -62,27 +72,27 @@ if (option.player.DataClear) {
 
 
 // #region 世界檔案清除
-if (option.world.SmallFileClear != 0) {
-	// 我自己 >> 清除前 12.9 GB (13,950,295,282 位元組) | 清除後 12.8 GB (13,754,439,835 位元組)
-	const WList = fs.readdirSync(`${PATH}/world/region`);
-	for (let index = 0; index < WList.length; index++) {
-		const S = fs.statSync(`${PATH}/world/region/${WList[index]}`);
-		if (S.size < option.world.SmallFileClear)
-			fs.unlinkSync(`${PATH}/world/region/${WList[index]}`);
-	}
-	// 我自己 >> 清除前 1.90 GB (2,045,203,288 位元組) | 清除後 1.85 GB (1,994,933,339 位元組)
-	const WList1 = fs.readdirSync(`${PATH}/world/DIM1/region`);
-	for (let index = 0; index < WList1.length; index++) {
-		const S = fs.statSync(`${PATH}/world/DIM1/region/${WList1[index]}`);
-		if (S.size < option.world.SmallFileClear)
-			fs.unlinkSync(`${PATH}/world/DIM1/region/${WList1[index]}`);
-	}
-	// 我自己 >> 清除前 789 MB (828,119,496 位元組) | 清除後 764 MB (801,674,298 位元組)
-	const WList2 = fs.readdirSync(`${PATH}/world/DIM-1/region`);
-	for (let index = 0; index < WList2.length; index++) {
-		const S = fs.statSync(`${PATH}/world/DIM-1/region/${WList2[index]}`);
-		if (S.size < option.world.SmallFileClear)
-			fs.unlinkSync(`${PATH}/world/DIM-1/region/${WList2[index]}`);
-	}
-}
+// if (option.world.SmallFileClear != 0) {
+// 	// 我自己 >> 清除前 12.9 GB (13,950,295,282 位元組) | 清除後 12.8 GB (13,754,439,835 位元組)
+// 	const WList = fs.readdirSync(`${PATH}/world/region`);
+// 	for (let index = 0; index < WList.length; index++) {
+// 		const S = fs.statSync(`${PATH}/world/region/${WList[index]}`);
+// 		if (S.size < option.world.SmallFileClear)
+// 			fs.unlinkSync(`${PATH}/world/region/${WList[index]}`);
+// 	}
+// 	// 我自己 >> 清除前 1.90 GB (2,045,203,288 位元組) | 清除後 1.85 GB (1,994,933,339 位元組)
+// 	const WList1 = fs.readdirSync(`${PATH}/world/DIM1/region`);
+// 	for (let index = 0; index < WList1.length; index++) {
+// 		const S = fs.statSync(`${PATH}/world/DIM1/region/${WList1[index]}`);
+// 		if (S.size < option.world.SmallFileClear)
+// 			fs.unlinkSync(`${PATH}/world/DIM1/region/${WList1[index]}`);
+// 	}
+// 	// 我自己 >> 清除前 789 MB (828,119,496 位元組) | 清除後 764 MB (801,674,298 位元組)
+// 	const WList2 = fs.readdirSync(`${PATH}/world/DIM-1/region`);
+// 	for (let index = 0; index < WList2.length; index++) {
+// 		const S = fs.statSync(`${PATH}/world/DIM-1/region/${WList2[index]}`);
+// 		if (S.size < option.world.SmallFileClear)
+// 			fs.unlinkSync(`${PATH}/world/DIM-1/region/${WList2[index]}`);
+// 	}
+// }
 // #endregion
